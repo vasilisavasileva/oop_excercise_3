9b032ad6ded7fa6a6010d8152e61c2a93c38bdfc
vasilisavasileva <55589269+vasilisavasileva@users.noreply.github.com>
2019-11-11 16:49:40 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -16,7 +16,6 @@ int main() {
 	setlocale(LC_ALL, "rus");
 	Figure* s;
 	std::vector<Figure*> v1;
-	double x1, x2, x3, x4, x5, x6, x7, x8, y1, y2, y3, y4, y5, y6, y7, y8;
 	printMenu();
 	while (true) {
 		std::cout << "Номер: ";
